92d7e55af3090230618fdeb21ed927a996e3a4af
Tarik Amehri <amehritarik@gmail.com>
2025-05-08 15:32:44 +0100

    notification hook

--- data/src/main.tsx
+++ data/src/main.tsx
@@ -6,8 +6,10 @@ import '@radix-ui/themes/styles.css';
 import './index.css';
 
 import App from './App.tsx';
+import { Toaster } from 'react-hot-toast';
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
+		<Toaster position="bottom-center" />
 		<Theme appearance="dark" accentColor="iris" grayColor="sand" radius="medium" scaling="100%">
 			<App />
 		</Theme>
